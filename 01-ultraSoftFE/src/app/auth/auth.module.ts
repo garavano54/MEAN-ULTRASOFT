0e5b960697c8e855d5f1426b9335beb651c77b78
Alejandro Garavano Viotti <garavano54@gmail.com>
2020-08-31 00:30:33 -0300

    Servicio Menu Dinamico

--- 01-ultraSoftFE/src/app/auth/auth.module.ts
+++ 01-ultraSoftFE/src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -10,7 +12,9 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   exports: [
     LoginComponent,
--- 01-ultraSoftFE/src/app/auth/login/login.component.ts
+++ 01-ultraSoftFE/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
     }
 
+    login() {
+      this.router.navigateByUrl('/');
+    }
 
 }
